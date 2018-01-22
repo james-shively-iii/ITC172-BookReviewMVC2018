a5fbc6a643a5b539aee81bfe357f5ee80e2a224b
James Shively III <james.shively-iii@seattlecentral.edu>
2018-01-22 11:46:59 -0800

    Updating BookReviewMVC2018 part 1
    Updating this repo with the new changes we did in class on January 22nd, 2018.  This will have add table data onto the homepage of the application.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookReviewMVC2018.Models;
 
 namespace BookReviewMVC2018.Controllers
 {
@@ -10,7 +11,11 @@ namespace BookReviewMVC2018.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            BookReviewDbEntities1 db = new BookReviewDbEntities1();
+            //var bks = (from b in db.Books
+                      //select b).ToList();
+
+            return View(db.Books.ToList());
         }
 
         public ActionResult About()
